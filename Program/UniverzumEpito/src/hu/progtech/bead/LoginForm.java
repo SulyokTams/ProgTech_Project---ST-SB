d878e33f98e61aae85e9aaf4a491d813af6e9a9c
Balazs Sulyok <sulykbazsi01@gmail.com>
2022-05-21 17:53:54 +0200

    List ScrollBar and Double Click

--- Program/UniverzumEpito/src/hu/progtech/bead/LoginForm.java
+++ Program/UniverzumEpito/src/hu/progtech/bead/LoginForm.java
@@ -1,4 +1,8 @@
 package hu.progtech.bead;
 
+import javax.swing.*;
+
 public class LoginForm {
+    private JList list1;
+    private JScrollBar scrollBar1;
 }
--- Program/UniverzumEpito/src/hu/progtech/bead/WelcomePage.java
+++ Program/UniverzumEpito/src/hu/progtech/bead/WelcomePage.java
@@ -1,9 +1,12 @@
 package hu.progtech.bead;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class WelcomePage implements ActionListener {
     JFrame frame = new JFrame();
@@ -11,9 +14,10 @@ public class WelcomePage implements ActionListener {
     JButton addButton = new JButton("Add Universe");
     JButton editButton = new JButton("Edit Universe");
     JButton deleteButton = new JButton("Delete Universe");
-
-    String[] data = {"one", "two", "three", "four"};
+    String[] data = {"one", "two", "three", "four","five","six","seven","eight"};
     JList universeList = new JList(data);
+    JScrollPane jcp = new JScrollPane(universeList);
+
     JLabel listLabel = new JLabel("Universe List");
 
     WelcomePage(String username){
@@ -22,12 +26,31 @@ public class WelcomePage implements ActionListener {
         welcomeLabel.setFont(new Font(null,Font.PLAIN,25));
         welcomeLabel.setText("Welcome " + "username!");
 
-        universeList.setBounds(800,70,370,680);
+        jcp.setBounds(800,70,370,680);
+
+        universeList.setFont(new Font(null,Font.PLAIN,35));
+        universeList.setBorder(new EmptyBorder(0,10, 10, 10));
+
+        universeList.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent evt) {
+                JList list = (JList)evt.getSource();
+                if (evt.getClickCount() == 2) {
+
+                    // Double-click detected
+                    int index = list.locationToIndex(evt.getPoint());
+                    System.out.println("index: "+index);
+                } else if (evt.getClickCount() == 3) {
+
+                    // Triple-click detected
+                    int index = list.locationToIndex(evt.getPoint());
+                    System.out.println("index: "+index);
+                }
+            }
+        });
+
         listLabel.setBounds(785,10,400,50);
         listLabel.setHorizontalAlignment(SwingConstants.CENTER);
         listLabel.setFont(new Font(null,Font.PLAIN,35));
-
-
 
         addButton.setBounds(25,150,150,50);
         addButton.setFocusable(false);
@@ -43,8 +66,9 @@ public class WelcomePage implements ActionListener {
         frame.add(addButton);
         frame.add(editButton);
         frame.add(deleteButton);
-        frame.add(universeList);
+        frame.add(jcp);
         frame.add(listLabel);
+
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setSize(1200,800);
         frame.setLayout(null);
