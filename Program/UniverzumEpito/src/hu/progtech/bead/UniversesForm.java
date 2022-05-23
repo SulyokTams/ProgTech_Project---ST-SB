fa0a23dc3e4f52c710efb0f7b35f7bd466368d70
SulyokTams <43856881+SulyokTams@users.noreply.github.com>
2022-05-23 06:30:45 +0200

    UniversesForm - GUI létrehozva

---
+++ Program/UniverzumEpito/src/hu/progtech/bead/UniversesForm.java
@@ -0,0 +1,13 @@
+package hu.progtech.bead;
+
+import javax.swing.*;
+
+public class UniversesForm {
+    private JPanel panelUniverses;
+    private JList listUniverses;
+    private JTextField textFieldUniverseName;
+    private JButton buttonDelete;
+    private JButton buttonEdit;
+    private JButton buttonNew;
+    private JButton button4;
+}
